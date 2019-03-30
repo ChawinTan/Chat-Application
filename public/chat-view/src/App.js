25d5801c85d5be72dea3428c8f3c0ea73fa90e4f
ChawinTan <chawin15@gmail.com>
2019-03-30 22:30:00 +0800

    set up chat view

--- public/chat-view/src/App.js
+++ public/chat-view/src/App.js
@@ -3,10 +3,48 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.ws = null;
+    this.state = {
+      newMsg: '',
+      chatContent: '',
+      email: '',
+      usermae: '',
+      joined: false
+    }
+    this.onJoin = this.onJoin.bind(this);
+  }
+
+  onJoin() {
+    this.setState({ joined: true });
+  }
+
   render() {
     return (
       <div className="App">
-        Hello
+        <div className="header">
+          <h2>Simple Chat App</h2>
+        </div>
+
+        <div className="chatbody">
+          
+        </div>
+
+        <div className="fields">
+        { !this.state.joined ?
+          <div>
+            <input className="username" />
+            <button onClick={this.onJoin}>Enter Chat!</button>
+          </div>
+          :
+          <div>
+            <input className="new-message" />
+            <button onClick={this.onJoin}>Send!</button>
+          </div>
+        }
+        </div>
       </div>
     );
   }
